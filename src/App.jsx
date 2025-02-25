a75e97eac4dbc221a715b60ba21971e2bdca0a1b
OksanaNova <o.nova0804@gmail.com>
2025-02-25 11:04:03 -0700

    MyList modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import './App.css'
 import MyList from './MyList';
 import MyMealsAndIngridients from './MyMealsAndIngridients';
 
 function App() {
 
+  const [mealPlans, setMealPlans] = useState([])
+
+  const addMeal = () => {
+    const newMeal = {
+      title: "Today is..."
+    }
+    setMealPlans([newMeal, ...mealPlans])
+    console.log(newMeal);
+  }
+
   return (
     <div className='App'>
-      <MyList />
+      <MyList addMeal={addMeal} mealPlans={mealPlans}/>
       <MyMealsAndIngridients />
 
     </div>
--- src/MyList.jsx
+++ src/MyList.jsx
@@ -1,9 +1,18 @@
-const MyList = () => {
+const MyList = ({addMeal, mealPlans}) => {
+
     return (<div>
         <div>
             <h1>Weekly Meal Plan Ideas</h1>
-            <button className="button-add">Add</button>
-            <button className="button-delete">Delete</button>
+            <button className="button-add" onClick={addMeal}>Add</button>
+        </div>
+
+        <div>
+            {mealPlans.map(({title}) => (
+                <div>
+                    <p>{title}</p>
+                    <button className="button-delete">Delete</button>
+                </div>
+            ))}
         </div>
     </div>)
 }
